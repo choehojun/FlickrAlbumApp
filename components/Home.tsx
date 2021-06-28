e5fe5969c5052a1f9ab1dea7b8f8ea9df38fbbb8
choehojun <86219758+choehojun@users.noreply.github.com>
2021-06-28 18:50:53 +0900

    라이브러리들 import 및 App.tsx 파일 구현

---
+++ components/Home.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import { StyleSheet, View, Button, Text } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+
+const Home = () => {
+
+}
+
+export default Home
---
+++ components/Slide.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect,  useState } from "react";
+import { View, Animated, Button, StyleSheet, Image, Text } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+
+const Slide = () => {
+
+}
+
+export default Slide
