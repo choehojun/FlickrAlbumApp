3edb4a9d9bf4d064ae69073be0ae25dc937fc0a0
choehojun <86219758+choehojun@users.noreply.github.com>
2021-06-28 19:55:37 +0900

    반복 애니메이션 구현

--- components/Slide.tsx
+++ components/Slide.tsx
@@ -4,11 +4,46 @@ import { Picker } from "@react-native-picker/picker";
 import Imgfile from "../assets/images/ImageForNews_26919_15786618897301054.png.webp";
 
 const Slide = ({navigation}) => {
+  const [fade] = useState(new Animated.Value(0))
+  const [flag, setFlag] = useState(false)
+
+  const fadeInAnimation = Animated.timing(
+    fade, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }
+  );
+  const fadeOutAnimation = Animated.timing(
+    fade, {
+      toValue: 0,
+      duration: 1000,
+      useNativeDriver: true,
+    }
+  );
+  const runAnimation = () => {
+    Animated.sequence([
+      fadeInAnimation,
+      Animated.delay(1000),
+      fadeOutAnimation
+    ]).start(() => {
+      setFlag(!flag)
+    })
+  }
+
+  useEffect(() => {
+    runAnimation()
+  }, [flag])
 
   return(
     <View style={styles.eachView}>
       <View style={styles.eachView}>
-        <Image source={Imgfile} />
+        <Animated.View style={{
+          opacity: fade,
+        }}
+        >
+          <Image source={Imgfile} />
+        </Animated.View>
       </View>
       <View style={styles.eachView}>
         <Button title="시작 화면으로"
