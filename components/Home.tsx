9e8e2aa879fab5b260829098707f593cab4c31d3
choehojun <86219758+choehojun@users.noreply.github.com>
2021-06-28 20:06:54 +0900

    Home/Slide 스크린에서 Picker로 이미지가 보여지는 시간 선택할 수 있도록 구현

--- components/Home.tsx
+++ components/Home.tsx
@@ -3,8 +3,12 @@ import { StyleSheet, View, Button, Text } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 
 const Home = ({navigation}) => {
+  const [title, setTitle] = useState('1')
   const isPressed = () => {
-    navigation.navigate('Slide')
+    const changedTitle = Number(title)
+    navigation.navigate('Slide', {
+      sec: changedTitle
+    })
   }
 
   return (
@@ -15,6 +19,21 @@ const Home = ({navigation}) => {
         </Text>
       </View>
       <View style={styles.viewTwo}>
+        <Picker style={{height: 200, width: 250}}
+                selectedValue={title}
+                onValueChange={(val) => setTitle(val)}>
+          <Picker.Item label='1초' value='1'/>
+          <Picker.Item label='2초' value='2'/>
+          <Picker.Item label='3초' value='3'/>
+          <Picker.Item label='4초' value='4'/>
+          <Picker.Item label='5초' value='5'/>
+          <Picker.Item label='6초' value='6'/>
+          <Picker.Item label='7초' value='7'/>
+          <Picker.Item label='8초' value='8'/>
+          <Picker.Item label='9초' value='9'/>
+          <Picker.Item label='10초' value='10'/>
+        </Picker>
+
         <Button title="시작"
                 onPress={isPressed}
         />
--- components/Slide.tsx
+++ components/Slide.tsx
@@ -4,8 +4,17 @@ import { Picker } from "@react-native-picker/picker";
 import Imgfile from "../assets/images/ImageForNews_26919_15786618897301054.png.webp";
 
 const Slide = ({navigation}) => {
+  const sec = navigation.getParam('sec')
   const [fade] = useState(new Animated.Value(0))
+  const [title, setTitle] = useState(String(sec))
+  const [seconds, setSeconds] = useState(sec * 1000)
   const [flag, setFlag] = useState(false)
+
+  const isChanged = (val: React.SetStateAction<string>) => {
+    const changedTitle = Number(val)
+    setSeconds(changedTitle * 1000)
+    setTitle(val)
+  }
 
   const fadeInAnimation = Animated.timing(
     fade, {
@@ -24,7 +33,7 @@ const Slide = ({navigation}) => {
   const runAnimation = () => {
     Animated.sequence([
       fadeInAnimation,
-      Animated.delay(1000),
+      Animated.delay(seconds),
       fadeOutAnimation
     ]).start(() => {
       setFlag(!flag)
@@ -42,8 +51,27 @@ const Slide = ({navigation}) => {
           opacity: fade,
         }}
         >
-          <Image source={Imgfile} />
+          <Image source={Imgfile}
+                 style={{width: 200, height: 200}}
+          />
         </Animated.View>
+        <Text>현재 시간: {seconds / 1000}초</Text>
+      </View>
+      <View style={styles.viewTwo}>
+        <Picker style = {{height: 200, width: 250}}
+                selectedValue={title}
+                onValueChange={(val) => isChanged(val)}>
+          <Picker.Item label='1초' value='1'/>
+          <Picker.Item label='2초' value='2'/>
+          <Picker.Item label='3초' value='3'/>
+          <Picker.Item label='4초' value='4'/>
+          <Picker.Item label='5초' value='5'/>
+          <Picker.Item label='6초' value='6'/>
+          <Picker.Item label='7초' value='7'/>
+          <Picker.Item label='8초' value='8'/>
+          <Picker.Item label='9초' value='9'/>
+          <Picker.Item label='10초' value='10'/>
+        </Picker>
       </View>
       <View style={styles.eachView}>
         <Button title="시작 화면으로"
