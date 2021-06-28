ae08a208e953577845aad5d99c0f66403fd5e905
choehojun <86219758+choehojun@users.noreply.github.com>
2021-06-28 19:20:04 +0900

    Home/Slide 스크린 View 구현

--- components/Home.tsx
+++ components/Home.tsx
@@ -2,8 +2,49 @@ import React, { useState } from "react";
 import { StyleSheet, View, Button, Text } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 
-const Home = () => {
+const Home = ({navigation}) => {
+  const isPressed = () => {
+    navigation.navigate('Slide')
+  }
 
+  return (
+    <View style={styles.mainView}>
+      <View style={styles.viewOne}>
+        <Text style={styles.textView}>
+          FlickrAlbumApp
+        </Text>
+      </View>
+      <View style={styles.viewTwo}>
+        <Button title="시작"
+                onPress={isPressed}
+        />
+      </View>
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  mainView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  viewOne: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  viewTwo: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center"
+  },
+  textView: {
+    marginTop: 100,
+    fontWeight: "bold",
+    fontSize: 35,
+    fontStyle: "italic"
+  }
+})
 
 export default Home
--- components/Slide.tsx
+++ components/Slide.tsx
@@ -1,9 +1,34 @@
 import React, { useEffect,  useState } from "react";
 import { View, Animated, Button, StyleSheet, Image, Text } from "react-native";
 import { Picker } from "@react-native-picker/picker";
+import Imgfile from "../assets/images/ImageForNews_26919_15786618897301054.png.webp";
 
-const Slide = () => {
+const Slide = ({navigation}) => {
 
+  return(
+    <View style={styles.eachView}>
+      <View style={styles.eachView}>
+        <Image source={Imgfile} />
+      </View>
+      <View style={styles.eachView}>
+        <Button title="시작 화면으로"
+                onPress={() => navigation.navigate('Home')}/>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  eachView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  viewTwo: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  }
+})
 
 export default Slide
